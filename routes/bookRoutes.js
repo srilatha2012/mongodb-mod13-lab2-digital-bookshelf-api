07fbde8270b034b3db2ab313b026928f15eb925c
srilatha2012 <srilathp2012@gmail.com>
2026-05-19 14:29:36 -0400

    Implemented all other CRUD operations for Book API

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,51 +1,65 @@
 const express = require("express");
 
-//create a new router object for movie routes
+//create a new router object for book routes
 const bookRouter = express.Router();
 
 const Book = require("../models/Book");
 
-//POST
+//CREATE - POST /api/books
 bookRouter.post("/", async (req, res) => {
     try {
         const newBook = await Book.create(req.body);
         res.status(201).json(newBook);
 
     } catch (error) {
-        res.status(500).send(error);
+        res.status(500).json({ message: error.message });
     }
 });
 
+//READ ALL - GET - /api/books
 bookRouter.get("/", async (req, res) => {
     try {
 
+        const allBooks = await Book.find();
+        res.json(allBooks);
     } catch (error) {
-        res.status(500).send(error);
+        res.status(500).json({ message: error.message });
     }
 
 });
 
+//READ ONE - GET /api/books/:id
 bookRouter.get("/:id", async (req, res) => {
     try {
-
+        const book = await Book.findById(req.params.id);
+        res.json(book);
     } catch (error) {
-        res.status(500).send(error);
+        res.status(500).json({ message: error.message });
     }
 });
 
+//UPDATE -PUT - /api/books/:id
 bookRouter.put("/:id", async (req, res) => {
     try {
-
+        const updatedBook = await Book.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            //show updated data and check validation rules
+            { new: true, runValidators: true }
+        );
+        res.json(updatedBook);
     } catch (error) {
-        res.status(500).send(error);
+        res.status(500).json({ message: error.message });
     }
 });
 
-bookRouter.delete("/:delete", async (req, res) => {
+//DELETE -DELETE /api/books/:id
+bookRouter.delete("/:id", async (req, res) => {
     try {
-
+        const deletedBook = await Book.findByIdAndDelete(req.params.id);
+        res.json(deletedBook);
     } catch (error) {
-        res.status(500).send(error);
+        res.status(500).json({ message: error.message });
     }
 });
 
